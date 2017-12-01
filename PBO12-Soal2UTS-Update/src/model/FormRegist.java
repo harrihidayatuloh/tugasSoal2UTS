a1a587ee69d51ad52e482772aa3666c2f2c56758
tomyskndr <32489340+tomyskndr@users.noreply.github.com>
2017-12-01 14:02:58 +0700

    Add files via upload

---
+++ PBO12-Soal2UTS-Update/src/model/FormRegist.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author user
+ */
+public class FormRegist {
+    
+    int noKtp;
+    String nama;
+
+    public int getNoKtp() {
+        return noKtp;
+    }
+
+    public void setNoKtp(int noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+
+    
+}
